d0a603ecc768be4ad886a43b23a82d22c391614d
Arpit Gupta <glex.qsd@gmail.com>
2015-05-11 19:44:54 -0400

    removing older files

--- profiler.py
+++
@@ -1,5 +0,0 @@
-import profile
-import pstats
-profile.run('import bgp_prefixSearch; bgp_prefixSearch.main()', 'profile.tmp')
-p = pstats.Stats('profile.tmp')
-p.sort_stats('cumulative').print_stats(10)
